3b13ef27cc73c46df8974cc3f881acab1b0ef891
Dev-Nagina Yadav <122589021+NAGINAY@users.noreply.github.com>
2024-05-06 09:45:35 +0530

    Add files via upload

---
+++ RESTAPI/helper/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+//get Base url
+if (!function_exists('getBaseURL')) {
+    function getBaseURL($url = '')
+    {
+        if ($url == '') {
+            return BASE_URL;
+        } else {
+            return BASE_URL . $url;
+        }
+    }
+}
+
+//getRaw Data : Json
+
+if(!function_exists('getRawData')){
+    function getRawData($typeArray = true)
+    {
+        $JSonObject = file_get_contents("php://input");
+        if($typeArray){
+            $formData =json_decode($JSonObject,$typeArray);
+            return $formData;
+        }else{
+                $formData = json_decode($JSonObject, $typeArray);
+                return $formData;
+        }
+    }
+}
+?>
